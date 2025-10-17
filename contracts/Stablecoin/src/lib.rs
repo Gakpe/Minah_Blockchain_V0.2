0e2c7d2aeebe0b7edb59b9774cd576441aad9c75
Ayoub Amer <ayouub.ameur@gmail.com>
2025-10-17 09:54:31 +0100

    feat: Implement constructor and fungible token interface for Stablecoin contract

--- contracts/Stablecoin/src/lib.rs
+++ contracts/Stablecoin/src/lib.rs
@@ -1,15 +1,32 @@
 #![no_std]
 
-use soroban_sdk::{contract, contractimpl};
+use soroban_sdk::{contract, contractimpl, Env, String};
+use stellar_macros::default_impl;
+use stellar_tokens::fungible::{Base, FungibleToken};
 
 #[contract]
 pub struct Stablecoin;
 
 #[contractimpl]
 impl Stablecoin {
+    pub fn __constructor(e: &Env) {
+        Base::set_metadata(
+            e,
+            6,
+            String::from_str(e, "USDC Mock Token"),
+            String::from_str(e, "USDC"),
+        );
+    }
+
     pub fn add(x: u32, y: u32) -> u32 {
         x.checked_add(y).expect("no overflow")
     }
+}
+
+#[default_impl]
+#[contractimpl]
+impl FungibleToken for Stablecoin {
+    type ContractType = Base;
 }
 
 #[cfg(test)]
