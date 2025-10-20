feab4e7b2128ca607e00f908e6a0a7ef605ac62c
Ayoub Amer <ayouub.ameur@gmail.com>
2025-10-20 11:51:08 +0100

    Ttest: add tests for release distribution logic and ensure proper state transitions

--- contracts/minah/src/tests/release.rs
+++ contracts/minah/src/tests/release.rs
@@ -1,8 +1,12 @@
-use soroban_sdk::{log, testutils::Address as _, Address, Env};
+use soroban_sdk::{
+    log,
+    testutils::{Address as _, Ledger},
+    Address, Env,
+};
 
 use crate::{
     tests::utils::{deploy_stablecoin_contract, mint_nft},
-    Minah, MinahClient,
+    InvestmentStatus, Minah, MinahClient, DISTRIBUTION_INTERVALS, ROI_PERCENTAGES,
 };
 
 #[test]
@@ -78,4 +82,224 @@ fn test_calculate_amount_to_release() {
     let amount_for_108_percent = client.calculate_amount_to_release(&percent_108);
 
     assert_eq!(amount_for_108_percent, expected_amount_to_release_108);
+}
+
+#[test]
+#[should_panic(expected = "DISTRIBUTION_NOT_READY_YET")]
+fn test_release_distribution_before_time_pass() {
+    let env = Env::default();
+    let owner = Address::generate(&env);
+    let stablecoin_address = deploy_stablecoin_contract(&env, &owner, 100_000_000 * 10i128.pow(6)); // Ensure huge supply
+    let receiver = Address::generate(&env);
+    let payer = Address::generate(&env);
+
+    env.mock_all_auths();
+    let contract_id = env.register(Minah, (&owner, &stablecoin_address, &receiver, &payer));
+    let client = MinahClient::new(&env, &contract_id);
+
+    // --- Setup Multiple Investors ---
+    let investor1 = Address::generate(&env); // Will hold 100 NFTs
+
+    let nft_amount_1: u32 = 100;
+
+    // Mint NFTs to investor1
+    mint_nft(
+        &env,
+        &client,
+        &investor1,
+        nft_amount_1,
+        &owner,
+        &stablecoin_address,
+        &contract_id,
+    );
+
+    let expected_current_supply = nft_amount_1;
+
+    // CHECK: Total supply should be nft_amount_1
+    assert_eq!(client.get_current_supply(), expected_current_supply);
+
+    // Start the chronometer
+    client.start_chronometer();
+
+    // CHECK: Chronometer should be started
+    let is_started = client.is_chronometer_started();
+    assert!(is_started);
+
+    // CHECK: Verify that the start time is set to the current ledger timestamp
+    let start_time = client.get_begin_date();
+    let current_time = env.ledger().timestamp();
+    assert_eq!(start_time, current_time);
+
+    // --- Attempt to release distribution immediately (should panic) ---
+    client.release_distribution();
+}
+
+#[test]
+fn test_release_distribution() {
+    let env = Env::default();
+    let owner = Address::generate(&env);
+    let stablecoin_address = deploy_stablecoin_contract(&env, &owner, 100_000_000 * 10i128.pow(6)); // Ensure huge supply
+    let receiver = Address::generate(&env);
+    let payer = Address::generate(&env);
+
+    env.mock_all_auths();
+    let contract_id = env.register(Minah, (&owner, &stablecoin_address, &receiver, &payer));
+    let client = MinahClient::new(&env, &contract_id);
+
+    // --- Setup Multiple Investors ---
+    let investor1 = Address::generate(&env); // Will hold 100 NFTs
+
+    let nft_amount_1: u32 = 100;
+
+    // Mint NFTs to investor1
+    mint_nft(
+        &env,
+        &client,
+        &investor1,
+        nft_amount_1,
+        &owner,
+        &stablecoin_address,
+        &contract_id,
+    );
+
+    let expected_current_supply = nft_amount_1;
+
+    // CHECK: Total supply should be nft_amount_1
+    assert_eq!(client.get_current_supply(), expected_current_supply);
+
+    // Start the chronometer
+    client.start_chronometer();
+
+    // CHECK: Chronometer should be started
+    let is_started = client.is_chronometer_started();
+    assert!(is_started);
+
+    // CHECK: Verify that the start time is set to the current ledger timestamp
+    let start_time = client.get_begin_date();
+    let current_time = env.ledger().timestamp();
+    assert_eq!(start_time, current_time);
+
+    // --- Prepare Payer (must approve the contract to spend the ROI stablecoin) ---
+    // The total possible ROI is for 10 distribution stages: 8*9 + 108 = 72 + 108 = 180%
+    // Total ROI amount to be transferred over time for 100 NFTs:
+    // (100 NFTs * PRICE * 180% of investment) = 100 * 1 * 1.8 = 180 stablecoins worth of value.
+    // Scaled: 180 * stablecoin_scale
+    let stablecoin_scale: i128 = 10i128.pow(6); // 6 decimals
+    let max_roi_amount = 180 * stablecoin_scale;
+
+    log!(
+        &env,
+        "Max ROI amount to be transferred to payer: {}",
+        max_roi_amount
+    );
+
+    // Transfer stablecoins to payer
+    let stablecoin_client = stablecoin::StablecoinClient::new(&env, &stablecoin_address);
+
+    stablecoin_client.transfer(&owner, &payer, &max_roi_amount);
+
+    // Initial check on claimed amount
+    let initial_claimed = client.see_claimed_amount(&investor1);
+    assert_eq!(initial_claimed, 0);
+
+    // --- Simulate Time Passage and Distribution ---
+    let timestamp = env.ledger().timestamp();
+
+    log!(
+        &env,
+        "Initial ledger timestamp: {}",
+        env.ledger().timestamp()
+    );
+
+    // Advance timestamp for the first distribution interval (6 months) and perform distributions
+    let new_timestamp = timestamp + DISTRIBUTION_INTERVALS[0]; // 6 months in seconds
+
+    env.ledger().set_timestamp(new_timestamp);
+
+    log!(
+        &env,
+        "New ledger timestamp after 6 months: {}",
+        env.ledger().timestamp()
+    );
+
+    // Calculate amount to be released for that first interval
+    let perceent_0 = ROI_PERCENTAGES[0];
+    let amount_to_release_0 = client.calculate_amount_to_release(&perceent_0);
+    let expected_amount_to_release_0: i128 =
+        (nft_amount_1 as i128 * client.get_nft_price() * perceent_0 / 100) * stablecoin_scale;
+
+    assert_eq!(amount_to_release_0, expected_amount_to_release_0);
+
+    log!(
+        &env,
+        "Amount to release for first interval: {}",
+        amount_to_release_0
+    );
+
+    // Approve the contract to spend from payer
+    stablecoin_client.approve(&payer, &contract_id, &amount_to_release_0, &100);
+
+    // Release distribution for the first interval
+    client.release_distribution();
+
+    // CHECK: claimed amount should be updated correctly
+    let claimed_after_first = client.see_claimed_amount(&investor1);
+    assert_eq!(claimed_after_first, amount_to_release_0);
+
+    // CHECK: state should be updated correctly
+    let state_after_first = client.get_current_state();
+    assert_eq!(state_after_first, InvestmentStatus::SixMonthsDone);
+
+    // Pass to the next intervals similarly...
+    let new_timestamp = timestamp + DISTRIBUTION_INTERVALS[1]; // Ten months in seconds
+    env.ledger().set_timestamp(new_timestamp);
+
+    let perceent_1 = ROI_PERCENTAGES[1];
+    let amount_to_release_1 = client.calculate_amount_to_release(&perceent_1);
+    let expected_amount_to_release_1: i128 =
+        (nft_amount_1 as i128 * client.get_nft_price() * perceent_1 / 100) * stablecoin_scale;
+
+    assert_eq!(amount_to_release_1, expected_amount_to_release_1);
+
+    log!(
+        &env,
+        "Amount to release for second interval: {}",
+        amount_to_release_1
+    );
+
+    // Approve the contract to spend from payer
+    stablecoin_client.approve(&payer, &contract_id, &amount_to_release_1, &100);
+
+    // Release distribution for the second interval
+    client.release_distribution();
+
+    // CHECK: claimed amount should be updated correctly
+    let claimed_after_second = client.see_claimed_amount(&investor1);
+    assert_eq!(
+        claimed_after_second,
+        amount_to_release_0 + amount_to_release_1
+    );
+
+    // CHECK: state should be updated correctly
+    let state_after_second = client.get_current_state();
+    assert_eq!(state_after_second, InvestmentStatus::TenMonthsDone);
+
+    // PASS the rest of the intervals by passing the timestamp to the end directly
+    let final_timestamp = timestamp + DISTRIBUTION_INTERVALS.last().unwrap();
+    env.ledger().set_timestamp(final_timestamp);
+
+    let approve_amount: i128 = max_roi_amount - (claimed_after_second); // Approve the remaining amount
+
+    stablecoin_client.approve(&payer, &contract_id, &approve_amount, &100);
+
+    // Release distribution for the final interval
+    client.release_distribution();
+
+    // CHECK: claimed amount should be updated correctly
+    let claimed_after_final = client.see_claimed_amount(&investor1);
+    assert_eq!(claimed_after_final, max_roi_amount);
+
+    // CHECK: state should be updated correctly
+    let state_after_final = client.get_current_state();
+    assert_eq!(state_after_final, InvestmentStatus::Ended);
 }
